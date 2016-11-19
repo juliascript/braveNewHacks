683f45bb53867ab41be7edc8dc6c8d9715d8c991
Julia <juliageist23@gmail.com>
2016-11-19 12:09:36 -0800

    prefab and scripts folder

---
+++ Unity/BraveNewHacks/Assets/Scripts/GenerateBodyScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateBodyScript : MonoBehaviour {
+
+	public GameObject personPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GameObject person = Instantiate (personPrefab);
+			GUIText text = person.GetComponent<GUIText> ();
+			text.text = "space";
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			GameObject person = Instantiate (personPrefab);
+			GUIText text = person.GetComponent<GUIText> ();
+			text.text = "right";
+		}
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			GameObject person = Instantiate (personPrefab);
+			GUIText text = person.GetComponent<GUIText> ();
+			text.text = "left";
+		}
+	}
+}
