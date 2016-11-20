04a6a3fc364db1cebc654ba5ac5402bd7a47ee5f
William W Wold <william01110111@gmail.com>
2016-11-19 16:32:47 -0800

    started working on ray tracing

---
+++ Unity/BraveNewHacks/Assets/deadGuyScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deadGuyScript : MonoBehaviour {
+
+	public GameObject textBox;
+
+	// Use this for initialization
+	void Start () {
+		textBox.showText ("Hi! I'm some text initalized at start");
+	}
+
+	public void isPointedAt() {
+
+		textBox.showText ("Hi! I'm some text.");
+	}
+	
+	// Update is called once per frame
+	/*void Update () {
+	
+	}*/
+}
